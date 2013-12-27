bd45278d2fa0358c42b09b3772c2aa85ff2d9b86
Treeskar <treeskar@gmail.com>
2013-12-27 13:23:57 +0200

    [+] add node server

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+
+app.get('*', function(req,res){
+	var file = 'build/index.html';
+	if(/.*\.(html|js|css|png|jpg|gif)$/.test(req.originalUrl)){
+		file = 'build/'+req.originalUrl.substr(1, req.originalUrl.length);
+	}
+	res.sendfile(file);
+});
+
+var port = process.env.PORT || 3000;
+app.listen(port);
