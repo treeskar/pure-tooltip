283cdb6d486995e9e6d5ac612df3d896118068c3
Treeskar <treeskar@gmail.com>
2013-12-31 19:31:58 +0200

    [+] demo tooltip style binding from diy controller to directive

--- static/app/directives.js
+++ static/app/directives.js
@@ -3,13 +3,58 @@
 
 	angular
 		.module('Directives', [])
-		.directive('header', ['Constant',function(Constant){
+		.directive('demoTooltip', ['$document', '$window', function($document, $window){
 			return {
 				restrict: 'A',
 				replace: true,
-				scope: {
-					activePage: '=page'
+				priority: 10,
+				link: function(scope, elm, attr){
+					scope.text = 'Pure CSS Tooltip';
+					scope.data = {};
+					scope.tooltipStyle = '';
+					// implement affix
+					if(typeof scope.tooltip === 'undefined'){
+						return;
+					}
+					if(scope.tooltip.affix){
+						$document.bind('scroll', function(e){
+							if($window.scrollY > 50){
+								elm.addClass('affix');
+							} else {
+								elm.removeClass('affix');
+							}
+						});
+					}
+					scope.$watch('tooltip', function(){
+						if(typeof scope.tooltip === 'undefined'){
+							return;
+						}
+						scope.tooltipStyle = ''+
+							'#wrapper header h1.tooltip-head {'+
+								'background-color: '+scope.tooltip.boxColor+';'+
+								'border-radius: '+scope.tooltip.boxRadius+'px;'+
+								'box-shadow: 0 0 '+scope.tooltip.boxShadowSize+'px '+scope.tooltip.boxShadowColor+';'+
+								'color: '+scope.tooltip.textColor+';'+
+								'font-size: '+Math.round(scope.tooltip.textSize*2)+'px;'+
+								'text-shadow: 0 0 '+scope.tooltip.textShadowSize+'px '+scope.tooltip.textShadowColor+';'+
+							'}'+
+							'#wrapper header h1.tooltip-head:after {' +
+								'border-width: '+scope.tooltip.triangleSize+'px;'+
+								'border-top-color: '+scope.tooltip.boxColor+';'+
+							'}';
+					}, true);
+
 				},
+				template: '' +
+					'<div>' +
+						'<style ng-bind-html="tooltipStyle"></style>' +
+						'<h1 '+
+							'class="tooltip-head" ' +
+							'contenteditable="true" ' +
+						'>{{ text }}</h1>'+
+					'</div>'
+			};
+		}])
 				link: function(scope, attr){
 				},
 				templateUrl: 'header-tpl.html'
