04409db2cf80b8b68ddd9fb744db87d5217046f8
Treeskar <treeskar@gmail.com>
2013-12-31 19:35:19 +0200

    [+] add tooltip settings to diy controller

--- static/app/controllers.js
+++ static/app/controllers.js
@@ -3,9 +3,24 @@
 
 	angular.module('Controllers', [])
 		.controller('demoCtrl', ['$scope', function($scope){
-			$scope.page = 'demo';
+			$scope.activePage = 'demo';
+
 		}])
 		.controller('diyCtrl', ['$scope', function($scope){
-			$scope.page = 'diy';
+			$scope.activePage = 'diy';
+			$scope.data = {};
+
+			$scope.tooltip = {
+				boxColor: 'rgba(0, 0, 0, 0.8)',
+				boxRadius: 0,
+				boxShadowColor: 'rgba(0, 0, 0, 0.5)',
+				boxShadowSize: 30,
+				triangleSize: 25,
+				textColor: '#FFF',
+				textSize: 15,
+				textShadowColor: '#000000',
+				textShadowSize: 16,
+				affix: true
+			};
 		}]);
 }());
